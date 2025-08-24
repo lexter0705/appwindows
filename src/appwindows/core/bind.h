8198a1c088d11c4e95d62695d9427a86a82d54e5
Alexey <88457801+lexter0705@users.noreply.github.com>
2025-08-24 17:41:47 +0300

    1) Refactored bind header 2) Removed comments from meson.build

--- src/appwindows/core/bind.h
+++ src/appwindows/core/bind.h
@@ -10,7 +10,7 @@ namespace core {
 void bind_window(const py::module &m);
 void bind_finder(const py::module &m);
 void bind_exceptions(const py::module &m);
-void bind_core(py::module &m);
+void bind_core(py::module &m, py::module &geometry, py::module &exceptions);
 
 }  // namespace windows
 }  // namespace appwindows
