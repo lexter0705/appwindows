fa232339914921f9fa59d5ff3d07eadddaafa72f
Alexey <88457801+lexter0705@users.noreply.github.com>
2025-08-24 17:58:04 +0300

    1) Refactored bind files

--- src/appwindows/appwindows.cc
+++ src/appwindows/appwindows.cc
@@ -12,14 +12,24 @@ PYBIND11_MODULE(appwindows, m) {
       "Python bindings for appwindows library\n\n"
       "The appwindows library solves the problem of working with a graphical "
       "shell on different operating systems.";
+
+  appwindows::core::bind_geometry(m);
+  appwindows::core::bind_exceptions(m);
+  appwindows::core::bind_window(m);
+  appwindows::core::bind_finder(m);
+
   auto geometry = m.def_submodule("geometry", "Geometry types and operations");
   auto exceptions = m.def_submodule("exceptions", "Exception types");
 
-  appwindows::core::bind_geometry(geometry);
-  appwindows::core::bind_exceptions(exceptions);
+  geometry.attr("Point") = m.attr("Point");
+  geometry.attr("Size") = m.attr("Size");
+  exceptions.attr("WindowDoesNotExistError") = m.attr("WindowDoesNotExistError");
+  exceptions.attr("InvalidSizeError") = m.attr("InvalidSizeError");
 
-  appwindows::core::bind_window(m);
-  appwindows::core::bind_finder(m);
+  m.attr("Point").cast<py::object>().dec_ref();
+  m.attr("Size").cast<py::object>().dec_ref();
+  m.attr("WindowDoesNotExistError").cast<py::object>().dec_ref();
+  m.attr("InvalidSizeError").cast<py::object>().dec_ref();
 
   m.def("get_finder", &appwindows::get_finder,
         "Get the window finder instance\n\n"
