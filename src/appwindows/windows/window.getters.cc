1040aed2f0e829be8bb1085f9f528122bb6c143a
Alexey <88457801+lexter0705@users.noreply.github.com>
2025-08-28 17:55:26 +0300

    1) Refactored Window.get_screenshot method

--- src/appwindows/windows/window.getters.cc
+++ src/appwindows/windows/window.getters.cc
@@ -62,9 +62,8 @@ py::array_t<unsigned char> WindowWindows::get_screenshot() {
   const auto old_bitmap = SelectObject(memory_dc, bitmap);
   const auto is_minimize = IsIconic(*window_) == TRUE;
   if (is_minimize) {
-    set_minimize(false);
-    to_background();
-    Sleep(1500);
+    ShowWindow(*window_, SW_SHOWNOACTIVATE);
+    Sleep(100);
   }
   PrintWindow(*window_, memory_dc, PW_RENDERFULLCONTENT);
   BITMAPINFOHEADER bitmap_info = {};
@@ -82,7 +81,7 @@ py::array_t<unsigned char> WindowWindows::get_screenshot() {
   DeleteObject(bitmap);
   DeleteDC(memory_dc);
   ReleaseDC(*window_, window_dc);
-  if (is_minimize) set_minimize(true);
+  if (is_minimize) ShowWindow(*window_, SW_MINIMIZE);;
   auto result_array = py::array_t<unsigned char>({height, width, 3});
   auto array_data = result_array.mutable_unchecked<3>();
   for (int y = 0; y < height; ++y)
