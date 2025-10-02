4c55dfee9a31dd1f6b418074eaf8cacd989dbce1
Alexey <88457801+lexter0705@users.noreply.github.com>
2025-10-02 20:58:05 +0300

    1) Refactored get_all_titles method in Finder

--- src/appwindows/core/finder.cc
+++ src/appwindows/core/finder.cc
@@ -8,7 +8,7 @@ namespace core {
 std::vector<std::unique_ptr<std::string>> Finder::get_all_titles() const {
   std::vector<std::unique_ptr<std::string>> titles;
   for (const auto& i : get_all_windows())
-    titles.push_back(std::make_unique<std::string>(*i->get_title()));
+    titles.push_back(i->get_title());
   return titles;
 }
 
