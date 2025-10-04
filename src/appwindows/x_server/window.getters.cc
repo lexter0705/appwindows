86e008bed112d51448669bf0ac1053e8f4c89b7f
lexter0705 <lexter0705@gmail.com>
2025-10-04 23:51:59 +0300

    1) Refactored get_process_id method in FInderXServer class

--- src/appwindows/x_server/window.getters.cc
+++ src/appwindows/x_server/window.getters.cc
@@ -85,6 +85,9 @@ std::unique_ptr<int> WindowXServer::get_process_id() const {
   XGetWindowProperty(
     display, window_, net_wm_pid, 0, 1, False, XA_CARDINAL, nullptr, nullptr,
     nullptr, nullptr, reinterpret_cast<unsigned char**>(&pid));
+  auto result = std::make_unique<int>(static_cast<int>(*pid));
+  XFree(pid)
+  XCloseDisplay(display);
   return std::make_unique<int>(static_cast<int>(*pid));
 }
 
