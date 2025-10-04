f2f19771e93e69ab6f1567a0e666b457327112a2
lexter0705 <lexter0705@gmail.com>
2025-10-05 00:17:22 +0300

    1) Refactored get_all_windows method in FInderXServer class

--- src/appwindows/x_server/finder.cc
+++ src/appwindows/x_server/finder.cc
@@ -63,20 +63,7 @@ std::vector<std::shared_ptr<core::Window>> FinderXServer::get_all_windows()
       Atom type;
       int format;
       unsigned long nitems, bytes_after;
-      unsigned char* data = nullptr;
-      if (XGetWindowProperty(display, children[i], net_wm_window_type, 0, ~0L,
-                             False, xa_atom, &type, &format, &nitems,
-                             &bytes_after, &data) == Success) {
-        if (type == xa_atom && data) {
-          Atom* types = (Atom*)data;
-          for (unsigned long j = 0; j < nitems; j++)
-            if (types[j] == net_wm_window_type_normal) {
-              windows.push_back(std::make_shared<WindowXServer>(children[i]));
-              break;
-            }
-          XFree(data);
-        }
-      }
+      windows.push_back(std::make_shared<WindowXServer>(children[i]));
     }
     if (children) XFree(children);
   }
