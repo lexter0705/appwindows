dff5b2a32d44b5d512e18742f380970a05e55a8b
Alexey <88457801+lexter0705@users.noreply.github.com>
2026-01-11 18:57:29 +0300

    1) Removed valid check on Window.get_title on MacOs for debug

--- src/macos/finder.cc
+++ src/macos/finder.cc
@@ -14,19 +14,31 @@ FinderMacOS::FinderMacOS() = default;
 
 std::vector<std::shared_ptr<core::Window>> FinderMacOS::get_all_windows()
     const {
-  CFArrayRef window_list =
-      CGWindowListCopyWindowInfo(kCGWindowListOptionAll, kCGNullWindowID);
+  CFArrayRef window_list = CGWindowListCopyWindowInfo(
+      kCGWindowListOptionOnScreenOnly | kCGWindowListExcludeDesktopElements,
+      kCGNullWindowID);
   std::vector<std::shared_ptr<core::Window>> windows;
   CFIndex count = CFArrayGetCount(window_list);
+
   for (CFIndex i = 0; i < count; ++i) {
     CFDictionaryRef window_info = reinterpret_cast<CFDictionaryRef>(
         CFArrayGetValueAtIndex(window_list, i));
+    CFNumberRef pid_ref = reinterpret_cast<CFNumberRef>(
+        CFDictionaryGetValue(window_info, kCGWindowOwnerPID));
     CFNumberRef window_id_ref = reinterpret_cast<CFNumberRef>(
         CFDictionaryGetValue(window_info, kCGWindowNumber));
+
+    if (!pid_ref || !window_id_ref) continue;
+
+    int pid;
+    CFNumberGetValue(pid_ref, kCFNumberIntType, &pid);
+    if (pid <= 0) continue;
+
     CGWindowID window_id;
     CFNumberGetValue(window_id_ref, kCFNumberIntType, &window_id);
     windows.push_back(std::make_shared<WindowMacOS>(window_id));
   }
+
   CFRelease(window_list);
   return windows;
 }
