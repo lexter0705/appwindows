bf9155fb888a3328012c22e09e5b9fbc21cbe4a1
lexter0705 <lexter0705@gmail.com>
2026-02-04 17:52:07 +0300

    1) Added PermissionDeniedException

--- src/core/exceptions/bind_exceptions.cc
+++ src/core/exceptions/bind_exceptions.cc
@@ -5,6 +5,7 @@
 #include "invalid_size.h"
 #include "window_does_not_valid.h" 
 #include "window_does_not_found.h" 
+#include "permission_denied.h" 
 
 namespace py = pybind11;
 
@@ -14,6 +15,7 @@ void bind_exceptions(py::module &m){
   py::register_exception<InvalidSizeException>(m, "InvalidSizeException");
   py::register_exception<WindowDoesNotValidException>(m, "WindowDoesNotValidException");
   py::register_exception<WindowDoesNotFoundException>(m, "WindowDoesNotFoundException");
+  py::register_exception<PermissionDeniedException>(m, "WindowDoesNotFoundException");
 }
 
 }  // namespace appwindows::core::geometry
